f556c3d1331be8f4ae3c78ebb87642586fe28749
Kirk True <kirk@mustardgrain.com>
2016-02-15 17:47:34 -0800

    Fixed for Linux and weird golang paths

--- git-multi-check-missing.sh
+++ git-multi-check-missing.sh
@@ -4,11 +4,29 @@ source $(dirname $0)/_git-multi-util.sh
 
 function check_dir() {
   local dir_name=$1
-  find $dir_name -depth 2  -type d '!' -exec test -e "{}/.git" ';' -print
+  local depth=$2
+
+  for child_dir in `find $dir_name -mindepth $depth -maxdepth $depth -type d '!' -exec test -e "{}/.git" ';' -print` ; do
+    parent_dir="`dirname $child_dir`"
+
+    # If the immediate parent of the directory contains a git
+    # repository, then let it pass.
+    if [ -d $parent_dir/.git ] ; then
+      continue
+    fi
+
+    # If the immediate parent of the directory is $GOPATH, then
+    # let it pass.
+    if [ "$GOPATH" != "" -a "$parent_dir" = "$GOPATH" ] ; then
+      continue
+    fi
+
+    echo $child_dir
+  done
 }
 
-check_dir "$LOCAL_DEV_DIR"
+check_dir "$LOCAL_DEV_DIR" 2
 
 if [ "$GOPATH" != "" ] ; then
-  check_dir "$GOPATH/src"
+  check_dir "$GOPATH/src" 3
 fi
