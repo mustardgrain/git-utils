35f10593938fae0b7f8311d9c6fbf3313f6e0bd6
Kirk True <kirk@mustardgrain.com>
2016-11-07 15:26:02 -0800

    Clean up of git-multi-branch.sh

--- git-multi-branch.sh
+++ git-multi-branch.sh
@@ -4,21 +4,15 @@ source $(dirname $0)/_git-multi-util.sh
 
 __git_init_parent_dirs
 
-# Show all of the branches of "-a" is passed in.
-show_all=false
-
-if [ "$1" = "-a" ] ; then
-  show_all=true
-fi
-
 for project_dir in "${git_parent_dirs[@]}" ; do
   cd "$project_dir"
 
   master_branch="`git remote show origin | grep "HEAD branch" | awk '{print $3}'`"
   current_branch="`git status | grep "On branch" | awk '{print $3}'`"
+  extra_branches=`git branch -a | grep -v $master_branch | wc -l`
 
-  if [ "$show_all" = true -o "$master_branch" != "$current_branch" ]; then
+  if [ "$master_branch" != "$current_branch" -o $extra_branches -gt 0 ]; then
     `which echo` "$project_dir:"
-    `which echo` "`git branch`"
+    `which echo` "`git branch -a -vv`"
   fi
 done
