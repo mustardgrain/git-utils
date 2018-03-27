d78e321880d07b62a21a1d6d2fcd3fc44b14505e
Kirk True <kirk@mustardgrain.com>
2018-03-27 13:57:19 -0700

    Pruning for all remotes, not just origin

--- git-multi-prune.sh
+++ git-multi-prune.sh
@@ -6,5 +6,9 @@ __git_init_parent_dirs 1
 
 for project_dir in "${git_parent_dirs[@]}" ; do
   cd "$project_dir"
-  git remote prune origin
+  remotes="`git remote | sort`"
+
+  for remote in ${remotes[@]} ; do
+    git remote prune $remote
+  done
 done
