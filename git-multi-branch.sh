4af51c40f45b9b7a1665551faf9c843d8d587642
Kirk True <kirk@mustardgrain.com>
2018-06-08 16:09:39 -0700

    Allow passing of parameters to git-multi-branch.sh that are run against git branch in each directory

--- git-multi-branch.sh
+++ git-multi-branch.sh
@@ -7,8 +7,8 @@ __git_init_parent_dirs 0
 for project_dir in "${git_parent_dirs[@]}" ; do
   cd "$project_dir"
 
-  if [ `git branch | wc -l` -gt 1 ]; then
+  if [ `git branch $@ | wc -l` -gt 1 ]; then
     `which echo` "$project_dir:"
-    `which echo` "`git branch`"
+    `which echo` "`git branch $@`"
   fi
 done
